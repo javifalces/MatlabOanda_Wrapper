6dacf59fd2aa969a5ac0afbe02e065aacb992ea4
Javier Falces <javifalces@gmail.com>
2014-06-02 09:51:59 +0200

    Add Java class PATCH
    Add patch

--- Example.m
+++ Example.m
@@ -7,11 +7,14 @@
 clear;
 clc;
 addpath 'libs'; % Json toolbox and urlread2
+
 token = 'your token for oanda';%my personal token
 account = 'account number';
 %% LOGN API
+%api = OandaAPI(token,'sandbox');%Practice
+api = OandaAPI(token,'practice',account);%Practice
 
-api = OandaAPI(token,'practice',account);
+
 
 
 %%
@@ -23,23 +26,9 @@ Prices = api.GetPrices({'EUR_USD' ,'USD_JPY'});
 %% History
 Hist = api.GetHistory('EUR_USD');
 
-%% Create Order - start trading
-[id,order1] = api.CreateOrder('EUR_USD' , 5, 'buy');%buy mkt order
-
-%% Get Order
-order = api.GetOrder(id);
-%% Close Order
-close = api.CloseOrder(id);
-%% Modify Order
-modif= api.ModifyOrder();
 %%
-ListAccounts = api.GetAccount()
-%%
-ListInstruments = api.GetInstruments()
-%% 
-Prices = api.GetPrices({'EUR_USD' ,'USD_JPY'});
-%% History
-Hist = api.GetHistory('EUR_USD');
+startdate = datenum('27-May-2014');
+Hist = api.GetHistory('USD_JPY' ,'M10', 251,startdate,now, 'midpoint',true  );
 
 %% Create Order - start trading
 [id,order1] = api.CreateOrder('EUR_USD' , 5, 'buy');%buy mkt order
@@ -57,12 +46,19 @@ Trades = api.GetListTrades();
 trade1 = api.GetTrade(Trades{1}.id);
 
 %% Modify Trade
-ret = api.ModifyTrade(Trades{1}.id , 123)
+
+% PATCH REQUEST
+%CHANGE jar in matlab ->   edit classpath.txt
+%# Comment these two lines and RESTART Matlab
+% #$matlabroot/java/jarext/axis2/commons-httpclient.jar
+% #$matlabroot/java/jarext/axis2/httpcore.jar
+
+
+ret = api.ModifyTrade(Trades{1}.id , 1.35,1.5,10);
 %% Close Trade
 deletetrade = api.CloseTrade(Trades{1}.id);
 
 %% Get History
 HistoryTrades = api.GetTransactionHistory();
-%% Get Prices Suscribe Streaming  - NOTWORKING
-PricesEurUSD = api.GetPricesSuscribe('EUR_USD');
-
+%% Get Prices Suscribe Streaming
+PricesEurUSD = api.GetPricesSuscribe('EUR_USD');% NOT Working Suscribtion
